901900b8e59b646f7ccb354424b73abb62ba7532
Marshall Zhang <mxzhang@college.harvard.edu>
2013-05-01 16:59:09 -0400

    Features compiles and runs omg

--- integral_image.py
+++ integral_image.py
@@ -1,23 +1,23 @@
 import numpy
 import scipy
-import training
+from training import Faces
 
 class Integrate:
 
-	def __init__(self):
-		original_img = self.faces.get_frame_vector("face_1.jpg")
-		integral_img = self.integral_image(original_img)
+	def __init__(self,img):
+		self.faces = Faces()
+		self.original_img = self.faces.get_frame_vector(img,False)
+		self.integral_img = self.integral_image()
 
 	
 	# x is an ndarray of the input 
 	# returns an ndarray that is an integral image / summed area table
-	def integral_image (self, x):
-		return x.cumsum(1).cumsum(0)
+	def integral_image (self):
+		return self.original_img.cumsum(1).cumsum(0)
 
 	# x, y are the coordinates of the corner of the box to be summed
 	# w, h are the width and height of the rectangle to be summed
 	def findIntegral(self, x,y,w,h):
-		
 		if (x>0 and y>0):
 			A = self.integral_img[x-1, y-1]
 		else:
