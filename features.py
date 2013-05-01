edaa0a6d5338b4c7b220a375831a44d2e8505f03
Marshall Zhang <mxzhang@college.harvard.edu>
2013-05-01 17:22:52 -0400

    Holy eff features.py is LIVE

--- features.py
+++ features.py
@@ -4,13 +4,13 @@ import Image
 from integral_image import Integrate
 from training import Faces
 
-class features:
+class Features:
 
 	def __init__(self):
 
 		# will be an array of all possible features for that image
 		self.f = []
-		self.feature_table = [1 for x in xrange(673680)]
+		self.feature_table = [None for x in xrange(673680)]
 		self.origin_x = 0
 		self.origin_y = 0
 		self.patch_scale = 1.0
@@ -23,46 +23,12 @@ class features:
 
 		self.init_helper()
 
-
-	# calculates value of the feature 
-	def compute_features (self, ind):
-		ind *= 5
-		feattype = self.feature_table[ind]
-		x = self.feature_table[ind+1]
-		y = self.feature_table[ind+2]
-		w = self.feature_table[ind+3]
-		h = self.feature_table[ind+4]
-
-		# Scale the feature to fit the current patch.
-		x = int(round(self.origin_x + x*self.patch_scale))
-		y = int(round(self.origin_y + y*self.patch_scale))
-		w = int(round(w*self.patch_scale))
-		h = int(round(h*self.patch_scale))
-
-		if feattype == 1:
-			return self.typeI(x, y, w, h)	
-		elif feattype ==2: 
-			return self.typeII(x, y, w, h)
-		elif feattype ==3:
-			return self.typeIII(x, y, w, h)
-		elif feattype ==4:
-			return self.typeIV(x, y, w, h)
-		else:
-			print 'Tried to use feature type:' + str(ind)
-
 	def init_helper(self):
-		i = 0
 		min_patch_side = 24 # smallest block size 
-		numfeatures = 53130
-
-		#nfeatures = 42310/100 
-
-		# for x in range(0, len(ind)):
-		# 	ind[x]= x
-
-		nfeatures = 4320000/100
 
 		ind = [x for x in xrange(134736)]
+
+		i = 0
 
 		# creates first type of feature of two rectangles stacked on top of each other
 		for w in xrange (1, min_patch_side+1):
@@ -75,7 +41,6 @@ class features:
 						self.feature_table[i+2] = y
 						self.feature_table[i+3] = w
 						self.feature_table[i+4] = h
-						print i
 						i+=5
 
 		# creates second type of feature of two rectangles side by side
@@ -88,7 +53,6 @@ class features:
 						self.feature_table[i+2] = y
 						self.feature_table[i+3] = w
 						self.feature_table[i+4] = h
-						print (" ",i)
 						i+=5
 
 		# creates third type of feature of three rectangles side by side
@@ -101,7 +65,6 @@ class features:
 						self.feature_table[i+2] = y
 						self.feature_table[i+3] = w
 						self.feature_table[i+4] = h
-						print ("  ",i)
 						i+=5
 
 		# creates fourth type of feature of four rectangles in checkerboard form
@@ -114,12 +77,13 @@ class features:
 						self.feature_table[i+2] = y
 						self.feature_table[i+3] = w
 						self.feature_table[i+4] = h
-						print ("   ",i)
 						i+=5
-
 		self.get_features(ind, self.f)
 
-
+	# takes in array ind and populates array f with features for that image
+	def get_features (self, ind, f):
+		for i in xrange (0, len(ind)):
+			f.append(self.compute_features(ind[i]))
 
 	# calculates value of the feature number ind
 	# returns feature value
@@ -137,6 +101,7 @@ class features:
 		w = int(round(w*self.patch_scale))
 		h = int(round(h*self.patch_scale))
 
+		# print x,y,w,h
 		if feattype == 1:
 			return self.typeI(self.pic, x, y, w, h)	
 		elif feattype ==2: 
@@ -147,7 +112,6 @@ class features:
 			return self.typeIV(self.pic, x, y, w, h)
 		else:
 			print 'Tried to use feature type:' + str(ind)
-
 
 
 	# sets region of interest with coordinates x, y and a width for specific features
@@ -167,48 +131,41 @@ class features:
 			self.patch_std = 1
 		else:
 			self.patch_std = math.sqrt(math.pow(mean,2)+meanSqr)
-		
 
-	# takes in array ind and populates array f with features for that image
-	def get_features (self, ind, f):
-		for i in xrange (0, len(ind)):
-			f.append(self.compute_features(ind[i]))
-			
 	"""
-
 	  Type I feature:
 	  	<w->
 	  ---- h
 	  ++++ h
-
 	"""
 	def typeI(self, integral_image, x, y, w, h):
 		sumU = integral_image.findIntegral(x,y,w,h)
 		sumD = integral_image.findIntegral(x,y+h,w,h)
-		return (sumD-sumU)/self.patch_std
-	
-	"""
+		# print int(sumD),int(sumU)
+		if sumD > 100000 or sumU > 100000:
+			print sumD,sumU
+		return (int(sumD) - int(sumU))	/ self.patch_std
 
+	"""
 	  Type II feature:
 	   <w-><w->
 	   ++++---- ^
 	   ++++---- h
 	   ++++---- v
-	 """
+	"""
 	def typeII(self, integral_image, x, y, w, h):
 
 		sumL = integral_image.findIntegral(x,y,w,h)
 		sumR = integral_image.findIntegral(x+w,y,w,h)
-		return (sumL-sumR)/self.patch_std
+
+		return (int(sumL)-int(sumR))/self.patch_std
 	
 	"""
-
 	  Type III feature:
 	 	<w-><w-><w->
 	   ++++----++++ ^
 	   ++++----++++ h
 	   ++++----++++ v
-	  
 	"""
 	def typeIII(self, integral_image, x, y, w, h):
 
@@ -216,10 +173,9 @@ class features:
 		sumC = integral_image.findIntegral(x+w,y,w,h)
 		sumR = integral_image.findIntegral(x+2*w,y,w,h)
 		# We have to account for the mean, since there are more (+) than (-).
-		return (sumL-sumC+sumR-self.patch_mean*w*h)/self.patch_std
+		return (int(sumL)-int(sumC)+int(sumR)-(self.patch_mean*w*h)) / self.patch_std
 	
 	"""
-
 	 Type IV feature:
 	 	<w-><w->
 	  ++++---- ^
@@ -234,7 +190,8 @@ class features:
 		sumRU = integral_image.findIntegral(x+w,y,w,h)
 		sumLD = integral_image.findIntegral(x,y+h,w,h)
 		sumRD = integral_image.findIntegral(x+w,y+h,w,h)
-		return (-sumLD+sumRD+sumLU-sumRU)/self.patch_std
+		return (-int(sumLD)+int(sumRD)+int(sumLU)-int(sumRU)) / self.patch_std
 		
-featuretest = features()
-featuretest.compute_features(1)
+featuretest = Features()
+
+print featuretest.f
