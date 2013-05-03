e492adcb7c7500a3e9291ee57e08bf1689bc4582
Marshall Zhang <mxzhang@college.harvard.edu>
2013-05-03 00:38:13 -0400

    ADABOOST WITH FEATURES WORKING

--- adaboost.py
+++ adaboost.py
@@ -11,6 +11,9 @@ class adaBoost:
 		self.data 			 = np.matrix([])
 		self.labels 		 = np.matrix([])
 		self.classifierArray = np.matrix([])
+		self.featuresMatrix = np.zeros((1,134736))
+
+
 
 	def get_frame_vector(self, video_frame, flatten=True):
 		im = Image.open(video_frame)
@@ -55,29 +58,35 @@ class adaBoost:
 		self.labels = ([1 for x in range(len(positiveSet))])
 		self.labels.extend([-1 for x in range(len(negativeSet))])
 
-	def guessClass(self,data,dim,threshold,inequality):
+		for img in self.data:
+			integralImageWithFeatures = Features(img)
+			print "dim of features:", len(integralImageWithFeatures.f)
+			self.featuresMatrix = np.vstack([self.featuresMatrix,integralImageWithFeatures.f])
+		self.featuresMatrix = np.delete(self.featuresMatrix,0,0)
+		n,m = np.shape(self.featuresMatrix)
+		print n,m
+
+	def guessClass(self,featuresMatrix,feature,threshold,inequality):
 
 		# get dimensions and make a return vector
-		n,m,o = np.shape(data)
-		classed = np.ones((n))
+		n,m = np.shape(self.featuresMatrix)
+		classed = (np.ones((1,n)))[0]
 
 		# if a data doesn't meet threshold make it -1
 		if inequality == '<=':
 			for img in range(0,n):
-				integralImageWithFeatures = Features(data[img])
-				if sum(integralImageWithFeatures.f) <= threshold:
+				if self.featuresMatrix[img][feature] <= threshold:
 					classed[img] = -1
 
 		else:
-				integralImageWithFeatures = Features(data[img])
-				if sum(integralImageWithFeatures.f) > threshold:
+			for img in range(0,n):
+				if self.featuresMatrix[img][feature] > threshold:
 					classed[img] = -1
 		return classed
 
 	def trainClassifier(self,data,labels,weights,steps):
 
 		# setup
-		featuresMatrix = np.matrix([])
 		labelMatrix = np.matrix(labels).T
 		n,m,o = np.shape(data)
 		bestClassifier = {}
@@ -90,46 +99,43 @@ class adaBoost:
 		# ON EACH COLUMN AND DO THE SAME THING WE'VE BEEN DOING!
 		# FUCK YEAH
 
-		for img in self.data:
-			integralImageWithFeatures = Features(img)
-			featuresMatrix = np.vstack([featuresMatrix,integralImageWithFeatures.f])
+		# the work
+		for feature in range(0,100):
+			# find min and max of x or y coordinates
+			rangeMin = self.featuresMatrix[:,feature].min()
+			rangeMax = self.featuresMatrix[:,feature].max()
 
-		# # the work
-		# for feature in range(0,len()):
-		# 	# find min and max of x or y coordinates
-		# 	rangeMin = dataMatrix[:,dim].min()
-		# 	rangeMax = dataMatrix[:,dim].max()
-		# 	stepSize = (rangeMax - rangeMin) / float(steps)
+			stepSize = (rangeMax - rangeMin) / float(steps)
 
-		# 	# for every little subdivision of the range
-		# 	for j in range(-1,steps + 1):
-		# 		for inequality in ['<=','>']:
+			# for every little subdivision of the range
+			for j in range(-1,steps + 1):
+				for inequality in ['<=','>']:
 
-		# 			# set the threshold value by incrementing one step over minrange
-		# 			# and classify based on the threshold
-		# 			threshold = (rangeMin + (j * stepSize))
-		# 			classGuess = self.guessClass(dataMatrix,dim,threshold,inequality)
+					# set the threshold value by incrementing one step over minrange
+					# and classify based on the threshold
+					threshold = (rangeMin + (j * stepSize))
+					classGuess = self.guessClass(self.featuresMatrix,feature,threshold,inequality)
+					# find the error in predictedClasses
+					errorArray = np.ones((n,1))
 
-		# 			# find the error in predictedClasses
-		# 			errorArray = np.ones((n,1))
+					# if wrong prediction, set to 0
+					for img in range(0,len(self.featuresMatrix)):
+						if classGuess[img] == labelMatrix[img]:
+							errorArray[img] = 0
 
-		# 			# if wrong prediction, set to 0
-		# 			for point in range(0,n):
-		# 				if classGuess[point] == labelMatrix[point]:
-		# 					errorArray[point] = 0
+					weightedError = np.matrix(weights).T * np.matrix(errorArray)
 
-		# 			weightedError = np.matrix(weights).T * np.matrix(errorArray)
+					# if weighted error is smallest, then put all our current 
+					# stuff in a dictionary
+					if weightedError < minError:
+						minError = weightedError
+						bestClassGuess = classGuess.copy()
+						bestClassifier['feature'] = feature
+						bestClassifier['threshold'] = threshold
+						bestClassifier['inequality'] = inequality
+						print "BESTCLASSIFIER", bestClassifier
 
-		# 			# if weighted error is smallest, then put all our current 
-		# 			# stuff in a dictionary
-		# 			if weightedError < minError:
-		# 				minError = weightedError
-		# 				bestClassGuess = classGuess.copy()
-		# 				bestClassifier['dim'] = dim
-		# 				bestClassifier['threshold'] = threshold
-		# 				bestClassifier['inequality'] = inequality
-
-		# return bestClassifier,minError,bestClassGuess
+		return bestClassifier,minError,bestClassGuess
 
 	def boost(self,maxFeatures):
 		weakClassGuessers = []
@@ -149,28 +155,38 @@ class adaBoost:
 			# train best classifier for these weights
 			bestClassifier,error,classGuess = self.trainClassifier(self.data,self.labels,weights,10)
 
-			print classGuess
+			print bestClassifier
+
 			# calculate weight of the classifier
 			alpha = float(math.log(1.0 - error) / max(error,1e-16))
-			print alpha
+			
 			bestClassifier['alpha'] = alpha
+
+			print "ALPHA", alpha
 
 			# add classifier to weakClassGuess
 			weakClassGuessers.append(bestClassifier)
 
 			# calculate new weights
 			exponent = np.multiply(1 * alpha * np.matrix(self.labels), classGuess)
+
+			print "EXPONENT", exponent
+
 			weights = np.multiply(weights,np.exp(exponent.T))
 			weights = weights * (1 / weights.sum())
 
+			print "WEIGHTS", weights
+
 			# update aggregateClassGuess
 			aggregateClassGuess = aggregateClassGuess + np.matrix((-1 * alpha * classGuess)).T
-			print np.matrix((-1 * alpha * classGuess)).T
+
 			# print aggregateClassGuess
 			# aggregateErrors
 			aggregateErrors = np.multiply(np.sign(aggregateClassGuess) != np.matrix(self.labels).T, np.ones((n,1)))
 			errorRate = aggregateErrors.sum() / n
 			# print aggregateErrors
+
+			print "ERRORRATE", errorRate
 
 			if errorRate == 0.0: 
 				print "NOERROR"
@@ -298,4 +314,3 @@ class cascade:
 adabooster = adaBoost()
 adabooster.loadData()
 adabooster.boost(30)
-print adabooster.classify([-3,1])
