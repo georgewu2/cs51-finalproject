b68d722afef7c59ca857e7dea4386d685d3b6d0e
George Wu <georgewu@college.harvard.edu>
2013-05-02 21:42:52 -0400

    added stuff

---
+++ alttraining.py
@@ -0,0 +1,101 @@
+import numpy as np
+import numpy.linalg as linalg
+import Image
+import os
+
+class Faces:
+
+	def __init__(self):
+		self.listfaces = []
+		self.faces = None
+		self.meanface = None
+		self.differencefaces = None
+		self.covmatrix = None
+		self.eigenfaces = None
+		self.weights = None
+
+	def get_frame_vector(self, video_frame, flatten=True):
+		im = Image.open(video_frame)
+
+		# convert to grayscale
+		im_gray = im.convert('L')
+
+		# convert to a matrix
+		im_matrix = np.matrix(im_gray)
+		if flatten == True:
+			return im_matrix.flatten('F')
+		else:
+			return im_matrix
+
+	def get_face_images(self):
+		# get all files of image directory
+		images = os.listdir(os.getcwd() + "/yalefaces")
+		
+		# get rid of the .DS_Store file
+		images.pop(0)
+
+		# add each vector to the list
+		for i in images:
+			self.listfaces.append(self.get_frame_vector("yalefaces/" + i).T)
+
+		# compress vector list into one matrix
+		self.faces = np.concatenate(self.listfaces, axis = 1)
+
+	def mean_face(self):
+		# np.mean IS SO SLOW WHYYYYYYY
+		self.meanface = np.mean(self.faces, axis = 1)
+
+	def difference_faces(self):
+		self.differencefaces = self.faces - self.meanface
+
+	def covariance(self):
+		self.covmatrix = self.differencefaces.T * self.differencefaces
+
+	def get_eigenfaces(self):
+
+		# get eigenvalues and eigenvectors
+		eigenvalues, eigenvectors = linalg.eig(self.covmatrix)
+		
+		# find the order of the eigenvalues in descending order
+		order = eigenvalues.argsort()[::-1]
+
+		# sort eigenvalues and eigenvectors according to most significant eigenvalues
+		eigenvalues = eigenvalues[order]
+		eigenvectors = eigenvectors[:,order]
+
+		# get only the 10 most significant eigenvectors
+		eigenvectors = eigenvectors[:,:10]
+
+		# create the eigenfaces
+		eigenfaces = self.differencefaces * eigenvectors
+		temp = eigenfaces.T
+		vectors = []
+		for face in temp:
+			length = linalg.norm(face)
+			nface = face / length
+			vectors.append(nface)
+		neigenfaces = np.concatenate(vectors).T
+		self.eigenfaces = neigenfaces
+
+	def get_weights(self):
+		self.weights = self.eigenfaces.T * self.faces
+
+	def main2(self):
+		self.get_face_images()
+		# print self.faces
+		self.mean_face()
+		# print self.meanface
+		self.difference_faces()
+		# print self.differencefaces
+		self.covariance()
+		# print self.covmatrix
+		self.get_eigenfaces()
+		# print self.eigenfaces
+		self.get_weights()
+		print self.weights
+		print "DONE"
+
+test = Faces()
+test.main2()
+# a = test.get_frame("stuff.jpg")
+# print test.matrix_to_vector(a)
