47276ec1169c97f1037d147ab448e80d2c442215
George Wu <georgewu@college.harvard.edu>
2013-05-04 12:16:59 -0400

    cleaned up code

--- recognize.py
+++ recognize.py
@@ -9,40 +9,31 @@ class Eigenfaces:
 		self.faces.train()
 		self.distances = []
 
+	# converts picture into vector and subtracts mean vector from it
 	def normalize (self, video_frame):
 		im_matrix = self.faces.get_frame_vector(video_frame).T
 		return im_matrix - self.faces.meanface
 
+	# projects vector into facespace
 	def project (self, face):
 		return self.faces.eigenfaces.T * face
 
+	# determines if there is a recognized face
 	def findface(self, a):
 		diff = self.faces.weights - a
+		
 		for i in diff.T:
 			self.distances.append(np.linalg.norm(i-a.T))
+		
 		if min(self.distances) > 10000:
 			print "Not recognized as a face"
+		
 		else:
 			index = self.distances.index(min(self.distances))
 			return index < 50
-			# print self.distances
-			# if index < 50:
-			# 	print "not smiling"
-			# else:
-			# 	print "smiling"
 
 	def classify (self, video_frame):
 		self.distances = []
 		normalized = self.normalize(video_frame)
 		projection = self.project(normalized)
 		self.findface(projection)
-		
-
-
-
-# test = Eigenfaces()
-# images = os.listdir(os.getcwd()+"/picturesofjames")
-# images.pop(0)
-# for i in images:
-# 	test.classify("picturesofjames/" +i)
-# test.classify("picturesofjames/img0004.jpg")
